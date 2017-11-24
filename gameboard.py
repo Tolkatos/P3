f2b919c083c4d1b0d7ea8c5b9e5d2bc938d67be6
Raphael Nuytten <raphael.nuytten@mail.com>
2017-11-24 11:14:12 +0100

    Suppression des position inutile contenu dans la class Gameboard

--- gameboard.py
+++ gameboard.py
@@ -6,16 +6,6 @@ class Gameboard:
             config = json.load(file)
         self.gameboard = config
         self.nbr_total_item = 3
-        self.pos_item_1_y = 0
-        self.pos_item_1_x = 0
-        self.pos_item_2_y = 0
-        self.pos_item_2_x = 0
-        self.pos_item_3_y = 0
-        self.pos_item_3_x = 0
-        self.pos_character_y = 0
-        self.pos_character_x = 0
-        self.pos_keeper_y = 13
-        self.pos_keeper_x = 14
 
     def display(self):
         """Fonction qui affiche le gameboard"""
