d258b90ca39e950a293a9472d78685eff0ba5731
Raphael Nuytten <raphael.nuytten@mail.com>
2017-12-09 09:29:56 +0100

    PEP8 / Docstring

--- item.py
+++ item.py
@@ -2,16 +2,19 @@ import random
 
 
 class Item:
+    """Class qui représente les objets
+    Attribut(2):
+    pos_y : ligne (défini aléatoirement dans la constructeur)
+    pos_x : sprite"""
+    def __init__(self):
+        self.pos_y = random.randint(0, 14)
+        self.pos_x = 0
 
-	def __init__(self):
-		self.pos_y = random.randint(0, 14)
-		self.pos_x = 0
-
-	def random_pos(self, gameboard):
-		"""Methode qui défini une position aléatoire"""
-		i = 0
-		while i == 0:
-			x = random.randint(0, 13)
-			if gameboard[self.pos_y][x] == 0:
-				i += 1
-		return x
+    def random_pos(self, gameboard):
+        """Methode qui défini une position aléatoire"""
+        i = 0
+        while i == 0:
+            pos_x = random.randint(0, 14)
+            if gameboard[self.pos_y][pos_x] == 0:
+                i += 1
+        return pos_x
--- keeper.py
+++ keeper.py
@@ -1,5 +1,10 @@
 class Keeper:
+    """Class qui représente le gardien
 
-	def __init__(self):
-		self.pos_y = 14
-		self.pos_x = 14
+    Attribut(2):
+    pos_y : ligne
+    pos_x : sprite"""
+
+    def __init__(self):
+        self.pos_y = 14
+        self.pos_x = 14
